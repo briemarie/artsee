a0c1a403e903744593d82d5e70282a5d6716311f
briemarie <thebriemarie@gmail.com>
2015-04-20 15:10:56 -0700

    Make hide function an IIFE so it runs automatically on search results

--- public/js/application.js
+++ public/js/application.js
@@ -2,10 +2,10 @@ function goBack() {
     window.history.back()
 }
 
-// This will be a function to hide imperfect search results
+// This hides imperfect search results
 function noDisplay() {
   if $('.bm-item-artists:not("Monet")')
     $('.bm-item-artists').hide();
-}
+}();
 
 
