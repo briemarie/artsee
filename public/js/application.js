82b945e01c80ec6b37f09113e106c06697f7e073
briemarie <thebriemarie@gmail.com>
2015-04-20 15:07:37 -0700

    Add JS function to filter out imperfect musuem api search results

--- public/js/application.js
+++ public/js/application.js
@@ -2,4 +2,10 @@ function goBack() {
     window.history.back()
 }
 
+// This will be a function to hide imperfect search results
+function noDisplay() {
+  if $('.bm-item-artists:not("Monet")')
+    $('.bm-item-artists').hide();
+}
+
 
